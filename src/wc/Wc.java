e2c91db64b93926d9165e2194c9912189bd8f258
jaky lin <you@example.com>
2018-09-14 20:18:23 +0800

    编写主函数进行测试

--- src/wc/Wc.java
+++ src/wc/Wc.java
@@ -22,6 +22,40 @@ public class Wc {
      */
     public static void main(String[] args) throws IOException {
         // TODO code application logic here
-        
+         wcFunction function=new wcFunction();
+        System.out.println("-c 返回文件 file.c 的字符数\n" +"-w 返回文件 file.c 的词的数目  \n" +"-l 返回文件 file.c 的行数");
+        System.out.println("-s   递归处理目录下符合条件的文件。\n" +"-a   返回更复杂的数据（代码行 / 空行 / 注释行）。");
+        System.out.println("请输入命令：（格式为：命令 文件路径）");
+        while(true){
+            
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input=br.readLine();
+        String[] commond=input.split(" ");
+        switch(commond[0]) {
+            case "-c": 
+                int num1=function.getCharNumber(commond[1]);
+                System.out.println("字符数："+num1);
+                break;
+            case "-w":
+                int num2=function.getWordNumber(commond[1]);
+                System.out.println("单词数："+num2);
+                break;
+            case "-l":
+                int num3=function.getLineNumber(commond[1]);
+                System.out.println("行数："+num3);
+                break;
+            case "-a":
+                int[] num4=function.extra_Function1(commond[1]);
+                System.out.println("空行："+num4[0]+" "+"代码行："+num4[1]+" "+"注释行："+num4[2]);
+                break;
+            case"-s":
+               function.scanFile(new File(commond[1]));
+                break;
+            default:
+                System.out.println("命令不正确");
+                break;
+            }
+    }
+    
     }
 }
